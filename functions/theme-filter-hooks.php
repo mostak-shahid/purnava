8ab4f8db02381f1a5d9a1ae65d564a318cbf8a3a
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-05-07 12:53:23 +0600

    1.0.8

--- functions/theme-filter-hooks.php
+++ functions/theme-filter-hooks.php
@@ -52,3 +52,27 @@ function mosacademy_additional_fields( $user_contact ) {
     return $user_contact;
 }
 add_filter( 'user_contactmethods', 'mosacademy_additional_fields' );
+
+
+/*Login Page*/
+function mos_loging_logo() { 
+?> 
+    <style type="text/css"> 
+    body.login div#login h1 a {
+        background-image: url(<?php echo get_template_directory_uri() . '/images/login-logo.png' ?>); 
+        padding-bottom: 30px;
+        background-size: unset; 
+        width: 100%;
+        height: 30px;
+        margin-top: 0;
+        margin-bottom: 0;
+    } 
+    </style>
+ <?php 
+}
+add_action( 'login_enqueue_scripts', 'mos_loging_logo' );
+
+function mos_login_url($url) {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'mos_login_url' );
