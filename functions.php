df47f3ab9368092380428f02f4d849d6979420da
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-05-07 12:37:15 +0600

    Login Logo and URL

--- functions.php
+++ functions.php
@@ -46,3 +46,25 @@ if (version_compare($GLOBALS['wp_version'], '5.0-beta', '>')) {
     // WP < 5 beta
     add_filter('gutenberg_can_edit_post_type', '__return_false');    
 }
+
+function mos_loging_logo() { 
+?> 
+	<style type="text/css"> 
+	body.login div#login h1 a {
+		background-image: url(<?php echo get_template_directory_uri() . '/images/login-logo.png' ?>); 
+		padding-bottom: 30px;
+		background-size: unset; 
+		width: 100%;
+		height: 30px;
+		margin-top: 0;
+		margin-bottom: 0;
+	} 
+	</style>
+ <?php 
+}
+add_action( 'login_enqueue_scripts', 'mos_loging_logo' );
+
+function mos_login_url($url) {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'mos_login_url' );
