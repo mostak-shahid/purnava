ff4b78dbe7359830c5946200d6fc43eda6a54ec8
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-05-15 13:03:15 +0600

    1.0.13

--- functions.php
+++ functions.php
@@ -39,10 +39,11 @@ require_once('functions/aq_resizer.php');
 require_once('functions/Mobile_Detect.php');
 require_once('functions/bs4navwalker.php');
 require_once('functions/breadcrumb.php');
-if (version_compare($GLOBALS['wp_version'], '5.0-beta', '>')) {    
+
+/*if (version_compare($GLOBALS['wp_version'], '5.0-beta', '>')) {    
     // WP > 5 beta
     add_filter('use_block_editor_for_post_type', '__return_false', 100);    
 } else {    
     // WP < 5 beta
     add_filter('gutenberg_can_edit_post_type', '__return_false');    
-}
+}*/
